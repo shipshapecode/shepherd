b83c0231e36b6b461114a4b8e00f7f8e2487d6d3
Preston Van Loon <plv2d@mtmail.mtsu.edu>
2014-10-16 13:07:13 -0500

    Fix for IE 9+
    Fixes #23 (+1 squashed commit)
    Squashed commits:
    [e66be62] Fix for IE 9+
    
    Fixes #23

--- js/shepherd.js
+++ js/shepherd.js
@@ -24,7 +24,7 @@
 
   matchesSelector = function(el, sel) {
     var matches, _ref1, _ref2, _ref3, _ref4;
-    matches = (_ref1 = (_ref2 = (_ref3 = (_ref4 = el.matches) != null ? _ref4 : el.matchesSelector) != null ? _ref3 : el.webkitMatchesSelector) != null ? _ref2 : el.mozMatchesSelector) != null ? _ref1 : el.oMatchesSelector;
+    matches = (_ref1 = (_ref2 = (_ref3 = (_ref4 = el.matches) != null ? _ref4 : el.msMatchesSelector) != null ? _ref3 : el.webkitMatchesSelector) != null ? _ref2 : el.mozMatchesSelector) != null ? _ref1 : el.oMatchesSelector;
     return matches.call(el, sel);
   };
 
