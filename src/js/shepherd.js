d46dd6abfd38ff45e90aa97746345c45e24aacd1
Winter Björn <bjoern.winter@d-velop.de>
2017-01-06 15:51:56 +0100

    Close button as HTML entity - adjust styling for available themes - removed useless properties

--- src/js/shepherd.js
+++ src/js/shepherd.js
@@ -379,7 +379,7 @@ class Step extends Evented {
     }
 
     if (this.options.showCancelLink) {
-      const link = createFromHTML("<a href class='shepherd-cancel-link'>✕</a>");
+      const link = createFromHTML("<a href class='shepherd-cancel-link'>&times;</a>");
       header.appendChild(link);
 
       this.el.className += ' shepherd-has-cancel-link';
