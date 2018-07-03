c3e3a737d3133c8cb5c75aee48bd8b68f11a9196
Robert Wagner <rwwagner90@gmail.com>
2018-07-03 09:52:51 -0400

    Use build stages

--- docs/welcome/js/welcome.js
+++ docs/welcome/js/welcome.js
@@ -32,7 +32,7 @@
     shepherd.addStep('including', {
       title: 'Including',
       text: 'Including Shepherd is easy! Just include popper.js, shepherd.js, and a Shepherd theme file.',
-      attachTo: '#hero-including-code left',
+      attachTo: '#hero-including-code bottom',
       buttons: [
         {
           text: 'Back',
@@ -47,7 +47,7 @@
     shepherd.addStep('example', {
       title: 'Example Shepherd',
       text: 'Creating a Shepherd is easy too! Just create Shepherd and add as many steps as you want. Check out the <a href="http://github.hubspot.com/shepherd">documentation</a> to learn more.',
-      attachTo: '#hero-example-code right',
+      attachTo: '#hero-example-code bottom',
       buttons: [
         {
           text: 'Back',
