00ace0149da6d9d219b1e9274bfb125fda2dab45
Winter Björn <bjoern.winter@d-velop.de>
2018-08-14 14:35:38 +0200

    Set default severity for stylelint so that the build- and linting process does not fail

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: 'stylelint-config-ship-shape'
+  extends: 'stylelint-config-ship-shape',
+  // Handle lint violations as warnings.
+  // Otherwise build fails
+  defaultSeverity: 'warning'
 };
