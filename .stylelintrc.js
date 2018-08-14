4ae302ac25a4d743c08faf63a0bfc68b6bd99bed
Winter Björn <bjoern.winter@d-velop.de>
2018-08-14 15:10:32 +0200

    remove unwanted property to make build fail on lin violations

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,3 @@
 module.exports = {
-  extends: 'stylelint-config-ship-shape',
-  // Handle lint violations as warnings.
-  // Otherwise build fails
-  defaultSeverity: 'warning'
+  extends: 'stylelint-config-ship-shape'
 };
