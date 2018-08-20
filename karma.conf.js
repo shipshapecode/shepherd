a28e5eef4d9035d7f9d04067fbdecd5e36151d50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-20 14:05:21 +0000

    Deploy shipshapecode/shepherd to github.com/shipshapecode/shepherd.git:gh-pages

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['chai', 'mocha'],
+    frameworks: ['mocha'],
 
     // list of files / patterns to load in the browser
     files: [
