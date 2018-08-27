125644daa75fe03516220dc42e85e64c51b57bdc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-27 18:12:11 +0000

    Deploy shipshapecode/shepherd to github.com/shipshapecode/shepherd.git:gh-pages

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -2,10 +2,18 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, './'),
+    inline: true,
+    open: true,
+    openPage: '/test/unit',
+    watchContentBase: true
+  },
   entry: ['./test/unit/setup.js', './test/unit/run.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'test/unit/dist'),
+    publicPath: 'test/unit/dist/',
   },
   mode: 'development',
   module: {
