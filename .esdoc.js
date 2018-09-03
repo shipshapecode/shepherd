0b08b160f2cf095b95e1895ced6d1efd0b416ab0
Brian Sipple <brsipple42@gmail.com>
2018-09-03 02:07:05 -0700

    add branding info to `.esdoc` config

--- .esdoc.js
+++ .esdoc.js
@@ -3,7 +3,15 @@ module.exports = {
   destination: './esdoc',
   plugins: [
     {
-      name: 'esdoc-standard-plugin'
+      name: 'esdoc-standard-plugin',
+      option: {
+        brand: {
+          title: 'Shepherd',
+          logo: './docs/assets/img/ship-shape-logo.png',
+          description: 'Guide your users through a tour of your app.',
+          repository: 'https://github.com/shipshapecode/shepherd.git',
+        },
+      },
     },
     {
       name: 'esdoc-ecmascript-proposal-plugin',
