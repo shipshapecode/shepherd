2f45b2533b3736cf45c11c79dee8a4b0e35f5977
Robert Wagner <rwwagner90@gmail.com>
2019-01-07 00:30:49 -0500

    Get copying styles via rollup working

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
+import autoprefixer from 'autoprefixer';
 import babel from 'rollup-plugin-babel';
 import { eslint } from 'rollup-plugin-eslint';
+import fs from 'fs';
 import license from 'rollup-plugin-license';
+import path from 'path';
+import postcss from 'postcss';
 import progress from 'rollup-plugin-progress';
 import filesize from 'rollup-plugin-filesize';
 import resolve from 'rollup-plugin-node-resolve';
+import sass from 'rollup-plugin-sass';
 import { uglify } from 'rollup-plugin-uglify';
 
 const PACKAGE = require('./package.json');
@@ -19,6 +24,9 @@ export default [
       name: 'Shepherd'
     },
     plugins: [
+      sass({
+        output: false
+      }),
       eslint(),
       babel({
         exclude: 'node_modules/**'
@@ -47,6 +55,24 @@ export default [
       uglify(),
       license({
         banner
+      }),
+      sass({
+        output(styles, styleNodes) {
+          fs.mkdirSync('dist/css', { recursive: true }, (err) => {
+            if (err) {
+              throw err;
+            }
+          });
+
+          styleNodes.forEach(({ id, content }) => {
+            const scssName = id.substring(id.lastIndexOf('/') + 1, id.length);
+            const name = scssName.split('.')[0];
+            fs.writeFileSync(`dist/css/${name}.css`, content);
+          });
+        },
+        processor: css => postcss([autoprefixer])
+          .process(css)
+          .then(result => result.css)
       }),
       progress(),
       filesize()
