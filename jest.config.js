925a729c7bcfdff1d45417d04715d7afd0c1a01f
Robert Wagner <rwwagner90@gmail.com>
2019-01-09 11:49:24 -0500

    Only collect coverage from src/js

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: null,
+  collectCoverageFrom: ['src/js/**.js'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
