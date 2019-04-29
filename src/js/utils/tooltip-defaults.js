e2e4397eabc4e9589951e6440c1c8999beb98a2a
Peter Homonnay <homonnay.peter@invensol.hu>
2019-04-29 17:10:06 +0200

    Added shepherd-popper class to top shepherd div

--- src/js/utils/tooltip-defaults.js
+++ src/js/utils/tooltip-defaults.js
@@ -1,11 +1,22 @@
 export const defaults = {
-  trigger: 'manual',
-  arrow: true,
-  animation: 'fade',
-  duration: 420,
-  flip: true,
-  animateFill: false, // https://atomiks.github.io/tippyjs/#animate-fill-option
-  interactive: true,  // https://atomiks.github.io/tippyjs/#interactive-option
-  hideOnClick: 'toggle', // https://atomiks.github.io/tippyjs/#hide-on-click-option
-  multiple: true  // https://atomiks.github.io/tippyjs/#multiple-option
+	trigger: 'manual',
+	arrow: true,
+	animation: 'fade',
+	duration: 420,
+	flip: true,
+	animateFill: false, // https://atomiks.github.io/tippyjs/#animate-fill-option
+	interactive: true,  // https://atomiks.github.io/tippyjs/#interactive-option
+	hideOnClick: 'toggle', // https://atomiks.github.io/tippyjs/#hide-on-click-option
+	multiple: true,  // https://atomiks.github.io/tippyjs/#multiple-option
+	popperOptions: {
+		modifiers: {
+			addShepherdClass: {
+				enabled: true,
+				fn: (data) => {
+					data.instance.popper.classList.add('shepherd-popper');
+					return data;
+				}
+			}
+		}
+	}
 };
