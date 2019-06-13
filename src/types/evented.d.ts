fe2ba3f71144b8d12ba66c30e9648e6fc6e756f8
superheri <30275781+superheri@users.noreply.github.com>
2019-06-13 09:31:40 -0400

    Add TypeScript definitions (#360)
    * WIP - Add Step types
    
    * Move a comment that was left behind while moving things around
    
    * Complete typings
    
    * Adapt some things to make it work with the demo from welcome.js like making options optional and editing the typings of the Shepherd.d.ts to define it as a class type not the instance of those classes

---
+++ src/types/evented.d.ts
@@ -0,0 +1,14 @@
+import Tour from "./tour";
+import Step from "./step";
+
+declare class Evented {
+    on(event: string, handler: Function, ctx: Step | Tour): void;
+
+    once(event: string, handler: Function, ctx: Step | Tour): void;
+
+    off(event: string, handler: Function): boolean | void;
+
+    trigger(event: string): void;
+}
+
+export = Evented;
