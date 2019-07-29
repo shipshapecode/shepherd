5afbaffd9aeac2f2c011a9b4b3b57e346fbb8429
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-29 13:14:38 +0000

    Deploy shepherdjs.dev to github.com/shipshapecode/shepherd.git:gh-pages

--- dist/js/shepherd.js
+++ dist/js/shepherd.js
@@ -1,4 +1,4 @@
-/*! shepherd.js 4.0.0-beta.17 */
+/*! shepherd.js 4.0.0-beta.18 */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
