1d40d61018beed812590b83d2d1d9382a42a077c
Genadi Sokolov <genadi@annoto.net>
2019-08-31 14:38:32 +0300

    fix tippy-popper-options unit test

--- test/unit/utils/tippy-popper-options.spec.js
+++ test/unit/utils/tippy-popper-options.spec.js
@@ -1,32 +1,29 @@
 import { makeAttachedTippyOptions } from '../../../src/js/utils/tippy-popper-options';
+import { Step } from '../../../src/js/step.jsx';
+import { Tour } from '../../../src/js/tour.jsx';
 
 describe('Tippy/Popper Options Utils', function() {
   describe('makeAttachedTippyOptions()', function() {
     it('passing tippyOptions.popperOptions sets nested values', function() {
-      const stepOptions =
-        {
-          options: {
-            tippyOptions: {
-              popperOptions: {
-                modifiers: {
-                  foo: 'bar',
-                  preventOverflow: {
-                    escapeWithReference: true
-                  }
-                }
+      const tour = new Tour();
+      const step = new Step(tour, {
+        tippyOptions: {
+          popperOptions: {
+            modifiers: {
+              foo: 'bar',
+              preventOverflow: {
+                escapeWithReference: true
               }
             }
-          },
-          styles: {
-            shepherd: ' shepherd'
           }
-        };
+        }
+      });
 
       const attachToOpts = {
         on: 'top'
       };
 
-      const tippyOptions = makeAttachedTippyOptions(attachToOpts, stepOptions);
+      const tippyOptions = makeAttachedTippyOptions(attachToOpts, step);
       expect(tippyOptions.popperOptions.modifiers.foo).toBe('bar');
       expect(tippyOptions.popperOptions.modifiers.preventOverflow.escapeWithReference).toBe(true);
     });
