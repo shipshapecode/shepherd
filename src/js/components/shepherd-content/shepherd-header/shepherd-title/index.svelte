67733b293ba1a32d9cc8cc1076a23bc07293d83f
Robert Wagner <rwwagner90@gmail.com>
2019-09-05 10:07:18 -0400

    Start on moving styles

---
+++ src/js/components/shepherd-content/shepherd-header/shepherd-title/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let labelId, styles, title;
+</script>
+
+<style>
+  h3 {
+    display: flex;
+    font-size: 1rem;
+    font-weight: normal;
+    flex: 1 0 auto;
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<h3
+  id="{labelId}"
+  class="{styles.title.trim()}"
+>
+    {title}
+</h3>
