9e08787a6cdc2828b3efe7372b7b68db7dbcd561
Chuck Carpenter <chuck.carpenter@me.com>
2019-09-18 15:17:25 -0700

    Add test on unique classes with multiple tours

--- test/cypress/integration/element-targeting.spec.js
+++ test/cypress/integration/element-targeting.spec.js
@@ -9,7 +9,7 @@ describe('Attaching tooltips to target elements in the DOM on each step', () => 
     cy.visit('/test/dummy/', {
       onLoad(contentWindow) {
         if (contentWindow.Shepherd) {
-          return Shepherd = contentWindow.Shepherd;
+          return (Shepherd = contentWindow.Shepherd);
         }
       }
     });
@@ -41,6 +41,48 @@ describe('Attaching tooltips to target elements in the DOM on each step', () => 
       cy.get('[data-test-hero-welcome]')
         .should('not.have.class', 'shepherd-target')
         .and('not.have.class', 'shepherd-enabled');
+    });
+  });
+
+  describe.only('Unique selectors with multiple Tours', function() {
+    let firstTour;
+    let secondTour;
+
+    beforeEach(() => {
+      firstTour = setupTour(Shepherd, {}, null, {
+        tourName: 'firstTour',
+        defaultStepOptions: {
+          classes: 'tour-test-1'
+        }
+      });
+      // setup a second tour with a unique name
+      secondTour = setupTour(Shepherd, {}, null, {
+        tourName: 'secondTour',
+        defaultStepOptions: {
+          classes: 'tour-test-2'
+        }
+      });
+    });
+
+    afterEach(() => {
+      firstTour.complete();
+      secondTour.complete();
+    });
+    it('applies default classes only on each individual tour', async function() {
+      firstTour.start();
+      secondTour.start();
+
+      cy.get('.shepherd-element').and(result => {
+        expect(result).to.have.lengthOf(2);
+      });
+
+      cy.get('.test-tour-1.shepherd-element').and(result => {
+        expect(result).to.have.lengthOf(1);
+      });
+
+      cy.get('.test-tour-2.shepherd-element').and(result => {
+        expect(result).to.have.lengthOf(1);
+      });
     });
   });
 });
