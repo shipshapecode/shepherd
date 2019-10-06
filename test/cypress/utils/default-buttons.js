d0bf019ad61242c9fda774ed7e8ddd3a1b211c3d
Alexey Pyltsyn <lex61rus@gmail.com>
2019-10-06 19:03:46 +0300

    Change label text for tests

--- test/cypress/utils/default-buttons.js
+++ test/cypress/utils/default-buttons.js
@@ -3,18 +3,18 @@ export default {
     classes: 'shepherd-button-secondary cancel-button',
     text: 'Exit',
     type: 'cancel',
-    label: 'Exit'
+    label: 'Exit label'
   },
   next: {
     classes: 'shepherd-button-primary next-button',
     text: 'Next',
     type: 'next',
-    label: 'Next'
+    label: 'Next label'
   },
   back: {
     classes: 'shepherd-button-primary back-button',
     text: 'Back',
     type: 'back',
-    label: 'Back'
+    label: 'Back label'
   }
 };
