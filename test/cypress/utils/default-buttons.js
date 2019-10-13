5954b7c7b3a95535772de2776f180ae5e581290b
Robert Wagner <rwwagner90@gmail.com>
2019-10-13 08:42:02 -0400

    Add label tests

--- test/cypress/utils/default-buttons.js
+++ test/cypress/utils/default-buttons.js
@@ -2,19 +2,16 @@ export default {
   cancel: {
     classes: 'shepherd-button-secondary cancel-button',
     text: 'Exit',
-    type: 'cancel',
-    label: 'Exit label'
+    type: 'cancel'
   },
   next: {
     classes: 'shepherd-button-primary next-button',
     text: 'Next',
-    type: 'next',
-    label: 'Next label'
+    type: 'next'
   },
   back: {
     classes: 'shepherd-button-primary back-button',
     text: 'Back',
-    type: 'back',
-    label: 'Back label'
+    type: 'back'
   }
 };
