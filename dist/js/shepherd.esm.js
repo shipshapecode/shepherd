0392e7350ababf674ed5a2d0673b5f23a1ac7c58
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-24 12:38:30 +0000

    Deploy shepherdjs.dev to github.com/shipshapecode/shepherd.git:gh-pages

--- dist/js/shepherd.esm.js
+++ dist/js/shepherd.esm.js
@@ -2167,7 +2167,7 @@ function set_attributes(node, attributes) {
       node.removeAttribute(key);
     } else if (key === 'style') {
       node.style.cssText = attributes[key];
-    } else if (descriptors[key] && descriptors[key].set) {
+    } else if (key === '__value' || descriptors[key] && descriptors[key].set) {
       node[key] = attributes[key];
     } else {
       attr(node, key, attributes[key]);
--- dist/js/shepherd.js
+++ dist/js/shepherd.js
@@ -2173,7 +2173,7 @@
         node.removeAttribute(key);
       } else if (key === 'style') {
         node.style.cssText = attributes[key];
-      } else if (descriptors[key] && descriptors[key].set) {
+      } else if (key === '__value' || descriptors[key] && descriptors[key].set) {
         node[key] = attributes[key];
       } else {
         attr(node, key, attributes[key]);
