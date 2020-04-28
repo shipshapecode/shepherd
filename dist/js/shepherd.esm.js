60085e8171184f23af0f9ea9c4d4a1d4d663f491
rwwagner90 <rwwagner90@users.noreply.github.com>
2020-04-28 03:33:55 +0000

    deploy: 4afa25d99bbc5394d6bc6c5c00d51c8c8b286259

--- dist/js/shepherd.esm.js
+++ dist/js/shepherd.esm.js
@@ -1,4 +1,4 @@
-/*! shepherd.js 7.1.4 */
+/*! shepherd.js 7.1.5 */
 
 function _defineProperties(target, props) {
   for (var i = 0; i < props.length; i++) {
--- dist/js/shepherd.js
+++ dist/js/shepherd.js
@@ -1,4 +1,4 @@
-/*! shepherd.js 7.1.4 */
+/*! shepherd.js 7.1.5 */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
