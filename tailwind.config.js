a25aa32ff8a98edfdd481d17ffcae1fad249213e
Robert Wagner <rwwagner90@gmail.com>
2021-06-21 19:34:06 -0400

    Update config for tailwind 2 (#1520)

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,19 @@ module.exports = {
       heading: ['GT Pressura', 'sans-serif']
     },
 
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem'
+    },
+
     maxWidth: {
       xxs: '13rem',
       xs: '20rem',
