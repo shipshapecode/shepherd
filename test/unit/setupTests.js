8cdd26e8e4d462bb737d95ef383b58cef50b9b47
Théodore BIADALA <theodore@biadala.net>
2022-08-29 14:06:15 +0200

    Don't display user errors during testing

--- test/unit/setupTests.js
+++ test/unit/setupTests.js
@@ -3,4 +3,13 @@ import 'jest-expect-message';
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
 
+global.console = {
+  ...console,
+  log: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn()
+};
+
 global.sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
