6c4b8aeab62895482ef2ae9a09de2335f3af63a4
Théodore BIADALA <theodore@biadala.net>
2022-08-29 14:50:47 +0200

    chore: make yarn lint:js pass

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   fixturesFolder: 'test/cypress/fixtures',
@@ -9,10 +9,10 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./test/cypress/plugins/index.js')(on, config)
+      return require('./test/cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:9002',
     specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'test/cypress/support/index.js',
-  },
-})
+    supportFile: 'test/cypress/support/index.js'
+  }
+});
