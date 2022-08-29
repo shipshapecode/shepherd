18e79286eea8965e6ccafb53c22cda4ab2fbac82
Théodore BIADALA <theodore@biadala.net>
2022-08-29 14:33:15 +0200

    Revert "Don't display user errors during testing"
    This reverts commit 8cdd26e8e4d462bb737d95ef383b58cef50b9b47.

--- test/unit/setupTests.js
+++ test/unit/setupTests.js
@@ -3,13 +3,4 @@ import 'jest-expect-message';
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
 
-global.console = {
-  ...console,
-  log: jest.fn(),
-  debug: jest.fn(),
-  info: jest.fn(),
-  warn: jest.fn(),
-  error: jest.fn()
-};
-
 global.sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
