c37d1aa0455ec087c1b8358136375c38e0401970
mspenc669 <matthew_spencer2@comcast.com>
2023-10-12 17:46:24 -0600

    Add section for inserting progress html

--- src/js/components/shepherd-footer.svelte
+++ src/js/components/shepherd-footer.svelte
@@ -4,6 +4,15 @@
   export let step;
 
   $: buttons = step.options.buttons;
+  $: progress = step.options.progress;
+
+  afterUpdate(() => {
+    if (isFunction(progress)) {
+      progress = progress();
+    }
+
+    if (progress) element.innerHTML = progress;
+  });
 </script>
 
 <style global>
@@ -11,22 +20,37 @@
     border-bottom-left-radius: 5px;
     border-bottom-right-radius: 5px;
     display: flex;
-    justify-content: flex-end;
+    flex-direction: column;
     padding: 0 0.75rem 0.75rem;
+    width 100%;
   }
 
   .shepherd-footer .shepherd-button:last-child {
     margin-right: 0;
   }
+
+  .shepherd-button-group {
+    display: flex;
+    justify-content: flex-end;
+    width 100%;
+  }
 </style>
 
 <footer class="shepherd-footer">
     {#if buttons}
+      <div class="shepherd-button-group">
         {#each buttons as config}
           <ShepherdButton
             {config}
             {step}
           />
         {/each}
+      </div>
+    {/if}
+    {#if progress}
+      <div
+        bind:this={element}
+        class="shepherd-progress">
+      </div>
     {/if}
 </footer>
