e0246c81ca8df63103c958beb4502e78d59b207d
Chuck Carpenter <chuck.carpenter@me.com>
2023-12-12 12:44:32 -0700

    👷‍ Update to use Astro on main landing site

---
+++ landing/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()]
+});
