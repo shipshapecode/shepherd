38af55ce7a926a121c43d49f8b441b3f60d5b10e
Chuck Carpenter <chuck.carpenter@me.com>
2023-12-21 16:34:12 -0700

    ✨ Add new pricing page with options and contact link

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://shepherdjs.dev',
   integrations: [mdx(), sitemap(), tailwind()],
   outDir: '../site'
 });
