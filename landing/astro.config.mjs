bb38bce44d4ad3cd85e983d6f2086faa711f7b89
Chuck Carpenter <chuck.carpenter@me.com>
2023-12-28 12:02:25 -0700

    📈 Add analytics and remove carbon ads

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shepherdjs.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown({ config: { forward: ['dataLayer.push'] } }),],
   outDir: '../site'
 });
