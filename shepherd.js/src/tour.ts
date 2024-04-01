92f00c28f61e6fe476c284dabb11eaa7e896c8ac
Robert Wagner <rwwagner90@gmail.com>
2024-03-31 23:03:16 -0400

    Fix lint

--- shepherd.js/src/tour.ts
+++ shepherd.js/src/tour.ts
@@ -13,7 +13,6 @@ import DataRequest from './utils/datarequest.ts';
 import { normalizePrefix, uuid } from './utils/general.ts';
 // @ts-expect-error TODO: not yet typed
 import ShepherdModal from './components/shepherd-modal.svelte';
-import type { StepNoOp, TourNoOp } from './utils/general.ts';
 
 interface Actor {
   actorId: number;
