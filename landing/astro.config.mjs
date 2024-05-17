2dc966ce30f566f8c0bcf0d7cfae7a70611db0d1
Chuck Carpenter <chuck.carpenter@me.com>
2024-05-17 16:21:08 -0700

    Landing: 👷 Remove Astro site config

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shepherdjs.dev',
   integrations: [
     mdx(),
     sitemap(),
