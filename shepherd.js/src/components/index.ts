0a521cd0efb4ba9cce7f491a390545e2646a0f7d
Robert Wagner <rwwagner90@gmail.com>
2024-05-20 14:41:12 -0400

    Generate types on postbuild

--- shepherd.js/src/components/index.ts
+++ shepherd.js/src/components/index.ts
@@ -1,9 +1,18 @@
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdButton } from './shepherd-button.svelte';
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdCancelIcon } from './shepherd-cancel-icon.svelte';
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdContent } from './shepherd-content.svelte';
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdElement } from './shepherd-element.svelte';
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdFooter } from './shepherd-footer.svelte';
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdHeader } from './shepherd-header.svelte';
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdModal } from './shepherd-modal.svelte';
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdText } from './shepherd-text.svelte';
+// @ts-expect-error TODO: figure out how to generate svelte declaration files
 export { default as ShepherdTitle } from './shepherd-title.svelte';
