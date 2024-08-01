e6b4f7eef480809529f4dfeaf40df1c82cdfcf25
Chuck Carpenter <charleswcarpenter3@gmail.com>
2024-08-01 14:14:31 -0700

    ✨ Landing: Add storyblok CMS

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,14 +1,38 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import storyblok from '@storyblok/astro';
 import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
+const { STORYBLOK_TOKEN } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.shepherdpro.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  redirects: {
-    '/blog': '/'
-  }
+  integrations: [
+    mdx(),
+    sitemap(),
+    storyblok({
+      accessToken: STORYBLOK_TOKEN,
+      apiOptions: {
+        region: 'us'
+      },
+      components: {
+        'all-articles': 'storyblok/AllArticles',
+        article: 'storyblok/Article'
+      }
+    }),
+    tailwind()
+  ],
+  // vite: {
+  //   plugins: [basicSsl()],
+  //   server: {
+  //     https: true
+  //   }
+  // }
+  // redirects: {
+  //   '/blog': '/'
+  // }
 });
