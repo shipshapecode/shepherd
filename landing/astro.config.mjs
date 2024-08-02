7d7e3c264f5d1c126a2edff18ed21539abaf6725
Chuck Carpenter <charleswcarpenter3@gmail.com>
2024-08-02 19:40:15 +0200

    ✨ Landing: Add storyblok CMS (#2938)
    * ✨ Landing: Add storyblok CMS
    
    * ➖ Landing: Remove unused deps
    
    * 🔥 Landing: Remove unused imports
    
    * ⬆ Update pnpm version to 9
    
    * 👷 Update prepare script to only build main js package
    
    * 👷 Add exclusion of static sites in build

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,14 +1,28 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import storyblok from '@storyblok/astro';
 import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite';
 
+const { STORYBLOK_TOKEN } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.shepherdpro.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  redirects: {
-    '/blog': '/'
-  }
+  integrations: [
+    mdx(),
+    sitemap(),
+    storyblok({
+      accessToken: STORYBLOK_TOKEN,
+      apiOptions: {
+        region: 'us'
+      },
+      components: {
+        'all-articles': 'storyblok/AllArticles',
+        article: 'storyblok/Article'
+      }
+    }),
+    tailwind()
+  ],
 });
