02f6140e0abef1878652a35a38d3ffc6e1bf6246
Chuck Carpenter <charleswcarpenter3@gmail.com>
2024-09-25 10:57:51 -0700

    🐛 Landing: Fix checkout issue with correct PAT (#2988)

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import { loadEnv } from 'vite';
 
 import vercel from '@astrojs/vercel/serverless';
 
