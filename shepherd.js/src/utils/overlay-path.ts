2563c01196764bf06523bf8634fadf4e11805645
AishwaryaAgr <Aishwarya@23484>
2024-10-02 10:18:13 +0530

    String continuations are not recommended

--- shepherd.js/src/utils/overlay-path.ts
+++ shepherd.js/src/utils/overlay-path.ts
@@ -39,19 +39,19 @@ export function makeOverlayPath({
     ? { topLeft: r, topRight: r, bottomRight: r, bottomLeft: r }
     : r;
 
-  return `M${w},${h}\
-H0\
-V0\
-H${w}\
-V${h}\
-Z\
-M${x + topLeft},${y}\
-a${topLeft},${topLeft},0,0,0-${topLeft},${topLeft}\
-V${height + y - bottomLeft}\
-a${bottomLeft},${bottomLeft},0,0,0,${bottomLeft},${bottomLeft}\
-H${width + x - bottomRight}\
-a${bottomRight},${bottomRight},0,0,0,${bottomRight}-${bottomRight}\
-V${y + topRight}\
-a${topRight},${topRight},0,0,0-${topRight}-${topRight}\
+  return `M${w},${h}
+H0
+V0
+H${w}
+V${h}
+Z
+M${x + topLeft},${y}
+a${topLeft},${topLeft},0,0,0-${topLeft},${topLeft}
+V${height + y - bottomLeft}
+a${bottomLeft},${bottomLeft},0,0,0,${bottomLeft},${bottomLeft}
+H${width + x - bottomRight}
+a${bottomRight},${bottomRight},0,0,0,${bottomRight}-${bottomRight}
+V${y + topRight}
+a${topRight},${topRight},0,0,0-${topRight}-${topRight}
 Z`;
 }
