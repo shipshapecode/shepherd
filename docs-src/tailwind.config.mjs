90409a03151abf6bdac2d11f1ef65a105cc653c5
Robert Wagner <rwwagner90@gmail.com>
2026-01-29 10:40:24 -0500

    Delete tailwind.config.mjs

--- docs-src/tailwind.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
